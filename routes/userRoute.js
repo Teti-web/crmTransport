48c7039fbffae01d1ea18f91dd7c3b166a7da6a4
Tetiana Topchii <tetiana.topchii@performance-media.pl>
2023-06-06 14:49:33 +0200

    innit commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,8 @@
 const express =require('express');
 const Router = require('express').Router;
 const router = new Router();
-const userController=  require('../controllers/userController');
-  const protect = require("../middleWare/authMiddleware");
+const userController= require('../controllers/userController');
+const protect = require("../middleWare/authMiddleware");
   
   //api/users/...
   router.post("/register", userController.registerUser);
@@ -11,8 +11,6 @@ const userController=  require('../controllers/userController');
   router.get("/getuser", protect, userController.getUser);
   router.get("/loggedin", userController.loginStatus);
   router.patch("/updateuser", protect, userController.updateUser);
-  router.patch("/changepassword", protect, userController.changePassword);
-  router.post("/forgotpassword", userController.forgotPassword);
-  router.put("/resetpassword/:resetToken", userController.resetPassword);
+ 
 
   module.exports = router;
